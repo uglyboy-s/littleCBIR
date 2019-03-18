deb2b466ddc6685403ec2f57da9b5c17c9472044
uglyboy-s <s-top@foxmail.com>
2019-03-18 18:24:09 +0800

    Create demo2.py

---
+++ demo2.py
@@ -0,0 +1,52 @@
+from keras.applications import ResNet50
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io
+
+app = flask.Flask(__name__)
+
+print('load model...')
+model = ResNet50(weights="imagenet")
+print('load done.')
+
+
+print('test model...')
+print(model.predict(np.zeros((1, 224, 224, 3))))
+print('test done.')
+
+
+def prepare_image(image, target):
+	if image.mode != "RGB":
+		image = image.convert("RGB")
+	image = image.resize(target)
+	image = img_to_array(image)
+	image = np.expand_dims(image, axis=0)
+	image = imagenet_utils.preprocess_input(image)
+	return image
+
+@app.route("/predict", methods=["POST"])
+def predict():
+        #global model
+	data = {"success": False}
+	if flask.request.method == "POST":
+		if flask.request.files.get("image"):
+			image = flask.request.files["image"].read()
+			image = Image.open(io.BytesIO(image))
+			image = prepare_image(image, target=(224, 224))
+			preds = model.predict(image)
+			results = imagenet_utils.decode_predictions(preds)
+			data["predictions"] = []
+			for (imagenetID, label, prob) in results[0]:
+				r = {"label": label, "probability": float(prob)}
+				data["predictions"].append(r)
+			data["success"] = True
+	return flask.jsonify(data)
+
+if __name__ == "__main__":
+	print(("* loading keras..."
+		"wait for a seconde please..."))
+	#load_model()
+	app.run()
